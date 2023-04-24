de1c10f44b472ab548f943dbb426a7cc231e683e
hamu4792game <112112847+hamu4792game@users.noreply.github.com>
2023-04-24 16:45:13 +0900

    一時退避

---
+++ Vector3.cpp
@@ -0,0 +1,41 @@
+#include "Vector3.h"
+
+Vector3::Vector3() {
+	this->x = 0.0f;
+	this->y = 0.0f;
+	this->z = 0.0f;
+}
+
+Vector3::Vector3(const float& X, const float& Y, const float& Z) {
+	this->x = X;
+	this->y = Y;
+	this->z = Z;
+}
+
+Vector3 Vector3::operator+(const Vector3& num) const {
+	Vector3 result;
+	result.x = this->x + num.x;
+	result.y = this->y + num.y;
+	result.z = this->z + num.z;
+	return result;
+}
+
+Vector3 Vector3::operator-(const Vector3& num) const {
+	Vector3 result;
+	result.x = this->x - num.x;
+	result.y = this->y - num.y;
+	result.z = this->z - num.z;
+	return result;
+}
+
+Vector3& Vector3::operator+=(const Vector3& num) {
+	*this = *this + num;
+
+	return *this;
+}
+
+Vector3& Vector3::operator-=(const Vector3& num) {
+	*this = *this - num;
+
+	return *this;
+}
